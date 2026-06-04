c6c606660199c8b680ece6c2388a1b13eb55c979
unknown <Projjwal@alliancescitizens.com>
2026-06-04 20:17:27 +0530

    Currency value changes for payment

--- controllers/walletController.js
+++ controllers/walletController.js
@@ -39,7 +39,7 @@ const getBalance = async (req, res) => {
 
 const createPayment = async (req, res) => {
     try {
-        const { amount, currency = 'USD' } = req.body;
+        const { amount, currency = 'ETH_BASE' } = req.body;
         const userId = req.user.id;
         const orderId = `ORDER-${userId}-${Date.now()}`;
 
